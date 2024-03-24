015aa87245aee8cff25d2f5e1ece3ab3945ac2b8
cvinodhkumaar <164789679+cvinodhkumaar@users.noreply.github.com>
2024-03-24 16:02:11 -0400

    commit

---
+++ RECRUITMENT_PORTAL_SCRIPT_FINAL_PROJECT.sql
@@ -0,0 +1,212 @@
+--SEEK_PER TABLE
+insert into Seek_per  values('A_1001','WilsonK','Adinolfi','1983-07-10',8579483232,'M','WilsonK.Adinolfi@gmail.com');
+insert into Seek_per  values('A_1002','Karthikeyan','AitSidi','1975-05-05',8579483233,'M','Karthikeyan.AitSidi@gmail.com');
+insert into Seek_per  values('A_1003','Sarah','Akinkuolie','1988-09-19',8579483234,'F','Sarah.Akinkuolie@gmail.com');
+insert into Seek_per  values('A_1004','Trina','Alagbe','1988-09-27',8579483235,'F','Trina.Alagbe@gmail.com');
+insert into Seek_per  values('A_1005','Carol','Anderson','1989-09-08',8579483236,'F','Carol.Anderson@gmail.com');
+insert into Seek_per  values('A_1006','Linda','Anderson','1977-05-22',8579483237,'F','Linda.Anderson@gmail.com');
+insert into Seek_per  values('A_1007','Colby','Andreola','1979-05-24',8579483238,'F','Colby.Andreola@gmail.com');
+insert into Seek_per  values('A_1008','Sam','Athwal','1983-02-18',8579483239,'M','Sam.Athwal@gmail.com');
+insert into Seek_per  values('A_1009','Linda','Bachiochi','1970-02-11',8579483240,'F','Linda.Bachiochi@gmail.com');
+insert into Seek_per  values('A_1010','Alejandro','Bacong','1988-01-07',8579483241,'M','Alejandro.Bacong@gmail.com');
+insert into Seek_per  values('A_1011','Rachael','Baczenski','1974-01-12',8579483242,'F','Rachael.Baczenski@gmail.com');
+insert into Seek_per  values('A_1012','Thomas','Barbara','1974-02-21',8579483243,'M','Thomas.Barbara@gmail.com');
+insert into Seek_per  values('A_1013','Hector','Barbossa','1988-07-04',8579483244,'M','Hector.Barbossa@gmail.com');
+insert into Seek_per  values('A_1014','FrancescoA','Barone','1983-07-20',8579483245,'M','FrancescoA.Barone@gmail.com');
+insert into Seek_per  values('A_1015','Nader','Barton','1977-07-15',8579483246,'M','Nader.Barton@gmail.com');
+insert into Seek_per  values('A_1016','Norman','Bates','1981-10-18',8579483247,'M','Norman.Bates@gmail.com');
+insert into Seek_per  values('A_1017','Kimberly','Beak','1966-04-17',8579483248,'F','Kimberly.Beak@gmail.com');
+insert into Seek_per  values('A_1018','Courtney','Beatrice','1970-10-27',8579483249,'F','Courtney.Beatrice@gmail.com');
+insert into Seek_per  values('A_1019','Becker','Renee','1986-04-04',8579483250,'F','Becker.Renee@gmail.com');
+insert into Seek_per  values('A_1020','Becker','Scott','1979-04-06',8579483251,'M','Becker.Scott@gmail.com');
+
+--FEEDBACK TABLE
+insert into Feed_back values ('F_1001','A_1001','Best Resume Search Engine');
+insert into Feed_back values ('F_1002','A_1001','Portal Works Great');
+insert into Feed_back values ('F_1003','A_1002','A Good Recruiting tool');
+insert into Feed_back values ('F_1004','A_1003','Best Resume Search Engine');
+insert into Feed_back values ('F_1005','A_1004','Portal Works Great');
+insert into Feed_back values ('F_1006','A_1005','A Good Recruiting tool');
+insert into Feed_back values ('F_1007','A_1006','Best Resume Search Engine');
+insert into Feed_back values ('F_1008','A_1007','Portal Works Great');
+insert into Feed_back values ('F_1009','A_1008','A Good Recruiting tool');
+insert into Feed_back values ('F_1010','A_1009','Best Resume Search Engine');
+insert into Feed_back values ('F_1011','A_1010','Portal Works Great');
+insert into Feed_back values ('F_1012','A_1011','A Good Recruiting tool');
+insert into Feed_back values ('F_1013','A_1012','Bad');
+insert into Feed_back values ('F_1014','A_1013','Best Resume Search Engine');
+insert into Feed_back values ('F_1015','A_1014','Portal Works Great');
+insert into Feed_back values ('F_1016','A_1015','A Good Recruiting tool');
+insert into Feed_back values ('F_1017','A_1016','Bad');
+insert into Feed_back values ('F_1018','A_1017','Bad');
+insert into Feed_back values ('F_1019','A_1018','Best Resume Search Engine');
+insert into Feed_back values ('F_1020','A_1019','Portal Works Great');
+insert into Feed_back values ('F_1021','A_1019','A Good Recruiting tool');
+
+--SEEK_RESUME TABLE
+insert into SEEK_RESUME values('RES_1001','A_1001','POST GRADUATE','COMPUTER SCIENCE','JAVA,PYTHON,DBMS',5,'COOKING');
+insert into SEEK_RESUME values('RES_1002','A_1002','POST GRADUATE','INFORMATION SYSTEMS','HTML, CSS, JAVA',4,'SPORTS');
+insert into SEEK_RESUME values('RES_1003','A_1003','GRADUATE','DATA ANALYTICS','R, PYTHON,DBMS',0,'NOVELS');
+insert into SEEK_RESUME values('RES_1004','A_1004','POST GRADUATE','CYBER SECURITY','SHELL SCRIPT,DBMS',2,'MUSIC');
+insert into SEEK_RESUME values('RES_1005','A_1005','GRADUATE','BUSINESS MANAGEMENT','MARKET RESEARCH, SALES AND MARKETING',1,'SPORTS');
+insert into SEEK_RESUME values('RES_1006','A_1006','POST GRADUATE','COMPUTER SCIENCE','JAVA,PYTHON,DBMS',3,'COOKING');
+insert into SEEK_RESUME values('RES_1007','A_1007','POST GRADUATE','INFORMATION SYSTEMS','HTML, CSS, JAVA',0,'MUSIC');
+insert into SEEK_RESUME values('RES_1008','A_1008','POST GRADUATE','DATA ANALYTICS','R, PYTHON,DBMS',0,'NOVELS');
+insert into SEEK_RESUME values('RES_1009','A_1009','GRADUATE','CYBER SECURITY','SHELL SCRIPT,DBMS',1,'MUSIC');
+insert into SEEK_RESUME values('RES_1010','A_1010','POST GRADUATE','BUSINESS MANAGEMENT','MARKET RESEARCH, SALES AND MARKETING',5,'COOKING');
+insert into SEEK_RESUME values('RES_1011','A_1011','GRADUATE','COMPUTER SCIENCE','JAVA,PYTHON,DBMS',4,'MUSIC');
+insert into SEEK_RESUME values('RES_1012','A_1012','POST GRADUATE','INFORMATION SYSTEMS','HTML, CSS, JAVA',0,'NOVELS');
+insert into SEEK_RESUME values('RES_1013','A_1013','GRADUATE','DATA ANALYTICS','R, PYTHON,DBMS',2,'COOKING');
+insert into SEEK_RESUME values('RES_1014','A_1014','GRADUATE','CYBER SECURITY','SHELL SCRIPT,DBMS',1,'SPORTS');
+insert into SEEK_RESUME values('RES_1015','A_1015','POST GRADUATE','BUSINESS MANAGEMENT','MARKET RESEARCH, SALES AND MARKETING',3,'COOKING');
+insert into SEEK_RESUME values('RES_1016','A_1016','POST GRADUATE','COMPUTER SCIENCE','JAVA,PYTHON,DBMS',4,'SPORTS');
+insert into SEEK_RESUME values('RES_1017','A_1017','GRADUATE','INFORMATION SYSTEMS','HTML, CSS, JAVA',4,'MUSIC');
+insert into SEEK_RESUME values('RES_1018','A_1018','GRADUATE','DATA ANALYTICS','R, PYTHON,DBMS',1,'MUSIC');
+insert into SEEK_RESUME values('RES_1019','A_1019','POST GRADUATE','CYBER SECURITY','SHELL SCRIPT,DBMS',2,'NOVELS');
+insert into SEEK_RESUME values('RES_1020','A_1020','GRADUATE','BUSINESS MANAGEMENT','MARKET RESEARCH, SALES AND MARKETING',1,'COOKING');
+
+--SEEK_LOGIN TABLE
+insert into SEEK_LOGIN values('adwilk','A_1001','adwilk_10','What is your pet name?','jimmy');
+insert into SEEK_LOGIN values('aikar','A_1002','aikar_11','What is your firstname?','aitsidi');
+insert into SEEK_LOGIN values('akisar','A_1003','akisar_12','How is your favourate player?','ronaldo');
+insert into SEEK_LOGIN values('alatri','A_1004','alatri_13','What is your university name?','Northeastern');
+insert into SEEK_LOGIN values('andcar','A_1005','andcar_14','What is your name?','andy');
+insert into SEEK_LOGIN values('andlin','A_1006','andlin_15','How is your favourate player?','messi');
+insert into SEEK_LOGIN values('athsam','A_1007','athsam_16','What is your pet name?','hazzle');
+insert into SEEK_LOGIN values('bachlin','A_1008','bachlin_17','What is your firstname?','bachiochi');
+insert into SEEK_LOGIN values('bacli','A_1009','bacli_18','How is your favourate player?','dhoni');
+insert into SEEK_LOGIN values('bacale','A_1010','bacale_19','What is your university name?','boston');
+insert into SEEK_LOGIN values('bacrac','A_1011','bacrac_20','What is your favorite movie?','mission impossible');
+insert into SEEK_LOGIN values('bartho','A_1012','bartho_21','How is your favourate player?','Ballayya');
+insert into SEEK_LOGIN values('barhec','A_1013','barhec_22','What is your pet name?','maple');
+insert into SEEK_LOGIN values('barfran','A_1014','barfran_23','What is your pet name?','tommy');
+insert into SEEK_LOGIN values('barna','A_1015','barna_24','What is your firstname?','barton');
+insert into SEEK_LOGIN values('batnor','A_1016','batnor_25','How is your favourate player?','mbappe');
+insert into SEEK_LOGIN values('bekim','A_1017','bekim_26','What is your firstname?','beak');
+insert into SEEK_LOGIN values('becour','A_1018','becour_27','How is your favourate player?','sachin');
+insert into SEEK_LOGIN values('rebec','A_1019','rebec_28','What is your pet name?','roony');
+insert into SEEK_LOGIN values('scobe','A_1020','scobe_29','What is your firstname?','scott');
+
+-- SEEK_ADD TABLE
+insert into SEEK_ADD values('A_1001','ADD_A_1001','Boston','Boston','8579483232');
+insert into SEEK_ADD values('A_1002','ADD_A_1002','Boston','Boston','8579483233');
+insert into SEEK_ADD values('A_1003','ADD_A_1003','Boston','Boston','8579483234');
+insert into SEEK_ADD values('A_1004','ADD_A_1004','Boston','Boston','8579483235');
+insert into SEEK_ADD values('A_1005','ADD_A_1005','Boston','Boston','8579483236');
+insert into SEEK_ADD values('A_1006','ADD_A_1006','Boston','Boston','8579483237');
+insert into SEEK_ADD values('A_1007','ADD_A_1007','Boston','Boston','8579483238');
+insert into SEEK_ADD values('A_1008','ADD_A_1008','Boston','Boston','8579483239');
+insert into SEEK_ADD values('A_1009','ADD_A_1009','Boston','Boston','8579483240');
+insert into SEEK_ADD values('A_1010','ADD_A_1010','Boston','Boston','8579483241');
+insert into SEEK_ADD values('A_1011','ADD_A_1011','Boston','Boston','8579483242');
+insert into SEEK_ADD values('A_1012','ADD_A_1012','Boston','Boston','8579483243');
+insert into SEEK_ADD values('A_1013','ADD_A_1013','Boston','Boston','8579483244');
+insert into SEEK_ADD values('A_1014','ADD_A_1014','Boston','Boston','8579483245');
+insert into SEEK_ADD values('A_1015','ADD_A_1015','Boston','Boston','8579483246');
+insert into SEEK_ADD values('A_1016','ADD_A_1016','Boston','Boston','8579483247');
+insert into SEEK_ADD values('A_1017','ADD_A_1017','Boston','Boston','8579483248');
+insert into SEEK_ADD values('A_1018','ADD_A_1018','Boston','Boston','8579483249');
+insert into SEEK_ADD values('A_1019','ADD_A_1019','Boston','Boston','8579483250');
+insert into SEEK_ADD values('A_1020','ADD_A_1020','Boston','Boston','8579483251');
+
+-- SEEK_EDU TABLE
+insert into SEEK_EDU values('A_1001','EDU_A_1001','POST GRADUATE','NEU',2000,90);
+insert into SEEK_EDU values('A_1002','EDU_A_1002','POST GRADUATE','BU',1998,87);
+insert into SEEK_EDU values('A_1003','EDU_A_1003','GRADUATE','MIT',2010,75);
+insert into SEEK_EDU values('A_1004','EDU_A_1004','POST GRADUATE','NEU',2001,85);
+insert into SEEK_EDU values('A_1005','EDU_A_1005','GRADUATE','Berklee',1997,77);
+insert into SEEK_EDU values('A_1006','EDU_A_1006','POST GRADUATE','Harvard',2000,90);
+insert into SEEK_EDU values('A_1007','EDU_A_1007','POST GRADUATE','NEU',1998,78);
+insert into SEEK_EDU values('A_1008','EDU_A_1008','POST GRADUATE','Berklee',2010,83);
+insert into SEEK_EDU values('A_1009','EDU_A_1009','GRADUATE','MIT',2010,92);
+insert into SEEK_EDU values('A_1010','EDU_A_1010','POST GRADUATE','BU',1998,76);
+insert into SEEK_EDU values('A_1011','EDU_A_1011','GRADUATE','NEU',2000,79);
+insert into SEEK_EDU values('A_1012','EDU_A_1012','POST GRADUATE','BU',2010,91);
+insert into SEEK_EDU values('A_1013','EDU_A_1013','GRADUATE','MIT',1997,80);
+insert into SEEK_EDU values('A_1014','EDU_A_1014','GRADUATE','MIT',2000,87);
+insert into SEEK_EDU values('A_1015','EDU_A_1015','POST GRADUATE','Harvard',1998,85);
+insert into SEEK_EDU values('A_1016','EDU_A_1016','POST GRADUATE','Berklee',2010,89);
+insert into SEEK_EDU values('A_1017','EDU_A_1017','GRADUATE','NEU',2011,92);
+insert into SEEK_EDU values('A_1018','EDU_A_1018','GRADUATE','BU',2003,70);
+insert into SEEK_EDU values('A_1019','EDU_A_1019','POST GRADUATE','MIT',2007,83);
+insert into SEEK_EDU values('A_1020','EDU_A_1020','GRADUATE','MIT',2005,91);
+
+--JOB_ PRO TABLE
+insert into JOB_PRO values('P_1001','Mark','Jenson','1988-09-27',8574489616,'M','Jenson.Mark@gmail.com');
+insert into JOB_PRO values('P_1002','Mitchell','Marshal','1989-09-08',8579991234,'M','Marshal.Mitchell@gmail.com');
+insert into JOB_PRO values('P_1003','Taylor','Sarah','1977-05-22',8470912823,'F','Sarah.Taylor@gmail.com');
+
+-- PRO_JOB_REQ TABLE
+insert into PRO_JOB_REQ  values ('J_1001','P_1001','POST_GRADUATE','HTML, CSS, JAVA',4,'Production Technician I','Active');
+insert into PRO_JOB_REQ  values ('J_1002','P_1002','GRADUATE','DBMS',3,'Sr. DBA','Active');
+insert into PRO_JOB_REQ  values ('J_1003','P_1003','GRADUATE','HTML, CSS, JAVA',2,'Software Engineer','Active');
+insert into PRO_JOB_REQ  values ('J_1004','P_1001','POST_GRADUATE','SHELL SCRIPT,DBMS',1,'Production Technician I','Active');
+insert into PRO_JOB_REQ  values ('J_1005','P_1002','POST_GRADUATE','SALES AND MARKETING',0,'Business Analyst','Active');
+insert into PRO_JOB_REQ  values ('J_1006','P_1003','GRADUATE','R, PYTHON,DBMS',2,'Data Analyst','Active');
+
+-- TEST_QUERIES TABLE
+Insert into TEST_QUERIES values('Q1','Function used for scatter plot','barplot','plot','hist','all of the above','plot');
+
+Commit;
+
+
+
+
+
+select * from SEEK_PER;
+select * from SEEK_RESUME;
+select * from FEED_BACK;
+select * from SEEK_LOGIN;
+select * from SEEK_ADD;
+select * from SEEK_EDU;
+select * from JOB_PRO;
+select * from PRO_JOB_REQ;
+select * from TEST_QUERIES;
+
+-- BELOW QUERY RETRIEVES THE INDIVIDUAL DETAILS AND EDUCATIONAL QUALIFICATION OF THE JOB SEEKERS
+
+SELECT p.SEEK_ID,SEEK_FNAME,p.SEEK_LNAME,p.SEEK_DOB,p.SEEK_CONTACT,p.SEEK_GENDER,p.SEEK_EMAIL,e.EDU_QUALIFICATION,e.EDU_UNIVERSITY,e.EDU_YEAROFPASS,e.EDU_PERCENT 
+FROM SEEK_PER p 
+JOIN SEEK_EDU e 
+ON p.SEEK_ID=e.SEEK_ID;
+
+--BELOW QUERY RETRIEVES JOB PROVIDERS DETAILS AND OPENINGS POSTED BY THEM
+
+SELECT p.PRO_ID,p.PRO_FNAME,p.PRO_LNAME,p.PRO_DOB,p.PRO_CONTACT,p.PRO_GENDER,p.PRO_EMAIL,q.JOB_ID,q.QUA_REQ,q.SKILLS_REQ,q.EXP_REQ,q.OPEN_POS,q.STATUS
+FROM JOB_PRO p
+JOIN PRO_JOB_REQ q
+ON p.PRO_ID=q.PRO_ID;
+
+
+
+-- BELOW QUERY RETRIEVES THE FEEDBACK PROVIDED BY THE JOB SEEKER 'WILSON K'
+
+SELECT p.SEEK_ID,p.SEEK_FNAME,p.SEEK_LNAME,p.SEEK_DOB,p.SEEK_CONTACT,p.SEEK_GENDER,p.SEEK_EMAIL,f.FEEDBACK_MESSAGE
+FROM SEEK_PER p 
+JOIN FEED_BACK f
+ON p.SEEK_ID=f.SEEK_ID 
+AND SEEK_FNAME='WilsonK';
+
+-- BELOW QUERY RETRIEVES THE LOGIN CREDENTIALS OF USERS (JOB SEEKERS)
+
+SELECT p.SEEK_ID,p.SEEK_FNAME,p.SEEK_LNAME,p.SEEK_EMAIL,l.SEEK_LOGIN_ID,l.SEEK_PWD,l.SEEK_QUES,l.SEEK_ANS
+FROM SEEK_PER p
+JOIN SEEK_LOGIN l
+ON p.SEEK_ID =l.SEEK_ID;
+
+-- BELOW QUERY RETRIEVES THE MAXIMUM,MINIMUM PERCENTAGE SCORED INDIVIDUAL DATA
+
+SELECT p.SEEK_ID,p.SEEK_FNAME,p.SEEK_LNAME,e.EDU_PERCENT
+FROM SEEK_PER p
+JOIN SEEK_EDU e
+ON p.SEEK_ID = e.SEEK_ID AND e.EDU_PERCENT in (SELECT max(EDU_PERCENT) from SEEK_EDU);
+
+SELECT p.SEEK_ID,p.SEEK_FNAME,p.SEEK_LNAME,e.EDU_PERCENT
+FROM SEEK_PER p
+JOIN SEEK_EDU e
+ON p.SEEK_ID = e.SEEK_ID AND e.EDU_PERCENT in (SELECT min(EDU_PERCENT) from SEEK_EDU);
+
+
+
+
+
